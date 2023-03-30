acaee60bf370c2fa5dd9d8172ece16982f3553af
thelicato <thelicato@users.noreply.github.com>
2023-03-30 14:30:25 +0200

    init: initial commit

---
+++ robodroid/__init__.py
@@ -0,0 +1,6 @@
+# Project Info
+__title__ = "robodroid"
+__version__ = "0.0.0"
+__description__ = (
+    "Manage and deploy Android machines with pre-defined behaviors for Cyber Range environments"
+)
