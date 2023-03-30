4809570ce596c305573ce34f74c0ce2c4400594c
thelicato <thelicato@users.noreply.github.com>
2023-03-30 15:21:41 +0200

    feat: add pre-commit and linting

--- robodroid/main.py
+++ robodroid/main.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
-def main():
+"""
+  Entrypoint file for RoboDroid with the CLI commands
+"""
+
+
+def main() -> None:
+    """
+    Entrypoint fn for RoboDroid
+    """
     print("Hello Robots!")
 
 
