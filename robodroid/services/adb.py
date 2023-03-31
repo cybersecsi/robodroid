7ba16cb18d6ce8d07b7df19a880b4e86488ad963
thelicato <thelicato@users.noreply.github.com>
2023-03-31 15:13:31 +0200

    feat: add service decorator for adb and test suite

--- robodroid/services/adb.py
+++ robodroid/services/adb.py
@@ -1,7 +1,32 @@
-from typing import List, cast
+from typing import List, Any, Callable, cast
+from functools import wraps
 from ppadb.client import Client as AdbClient
 from ppadb.device import Device
 from robodroid.utils import logger
+
+
+def validate_connection(return_value: Any) -> Callable[..., Callable[..., Any]]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def wrapper(*args: Any, **kw: Any) -> Any:
+            adb_instance: Adb = args[0]
+            if not adb_instance.device:
+                logger.error("No device connected!")
+                return return_value
+            return func(*args, **kw)
+
+        return wrapper
+
+    return decorator
+
+
+def dump_stdout(connection: Any) -> None:
+    while True:
+        data = connection.read(1024)
+        if not data:
+            break
+        print(data.decode("utf-8"))
+    connection.close()
 
 
 class Adb:
@@ -13,7 +38,6 @@ class Adb:
         self.host = host
         self.port = port
         self.client = AdbClient(host=host, port=port)
-        self.device = None
 
     def get_adb_version(self) -> int:
         return self.client.version()
@@ -24,27 +48,23 @@ class Adb:
     def connect(self, device_name: str) -> None:
         self.device = cast(Device, self.client.device(device_name))
 
+    @validate_connection("")
+    def get_device_arch(self) -> str:
+        return self.device.get_properties()["ro.product.cpu.abi"]
+
+    @validate_connection(False)
     def is_apk_installed(self, package_name: str) -> bool:
-        if not self.device:
-            logger.error("No device connected!")
-            return False
         return self.device.is_installed(package_name)
 
+    @validate_connection(None)
     def install(self, apk_path: str) -> None:
-        if not self.device:
-            logger.error("No device connected!")
-            return
         self.device.install(apk_path)
 
+    @validate_connection(None)
     def uninstall(self, package_name: str) -> None:
-        if not self.device:
-            logger.error("No device connected!")
-            return
         if self.is_apk_installed(package_name):
             self.device.uninstall(package_name)
 
+    @validate_connection(None)
     def shell_cmd(self, cmd: str) -> None:
-        if not self.device:
-            logger.error("No device connected!")
-            return
-        self.device.shell(cmd)
+        self.device.shell(cmd, handler=dump_stdout)
