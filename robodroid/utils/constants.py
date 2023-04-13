8315e8e35c9fca70f735b505e0fdd58098bbff3b
thelicato <thelicato@users.noreply.github.com>
2023-04-13 12:13:41 +0200

    fix: wrong colors

--- robodroid/utils/constants.py
+++ robodroid/utils/constants.py
@@ -16,8 +16,8 @@ ROBODROID_GIT_REPO = "https://github.com/cybersecsi/robodroid"
 ROBODROID_LIBRARY_GIT_REPO = "https://github.com/cybersecsi/robodroid-library"
 
 # Colors
-SUCCESS_COLOR = "\033[92m"
-ERROR_COLOR = "\033[91m"
-DEBUG_COLOR = "\033[93m"
+SUCCESS_COLOR = "\033[32m"
+ERROR_COLOR = "\033[31m"
+DEBUG_COLOR = "\033[33m"
 BOLD_FORMAT = "\033[1m"
 END_FORMAT = "\033[0m"
